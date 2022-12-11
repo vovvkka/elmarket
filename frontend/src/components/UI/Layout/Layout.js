5ae9ea942d38b375555ee1d1aaba0237bab18db3
lera <lera.24054@gmail.com>
2022-12-11 11:23:16 +0600

    Background fix

--- frontend/src/components/UI/Layout/Layout.js
+++ frontend/src/components/UI/Layout/Layout.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from "../Header/Header";
+import {useLocation} from "react-router-dom";
 
 const Layout = ({children}) => {
+    const location = useLocation();
+
     return (
         <>
-            <Header/>
+            {location.pathname === '/' ?
+                <div className='background-wrapper'>
+                    <Header/>
+                </div> : <Header/>
+            }
             <div className='main'>
                     {children}
             </div>

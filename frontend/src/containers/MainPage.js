44b02e4c3d3fa3f29c3c415cc351076d70173483
Vladimir Baltiev <v.golem2288@gmail.com>
2022-12-13 13:49:46 +0600

    Added Instruments component with media queries

--- frontend/src/containers/MainPage.js
+++ frontend/src/containers/MainPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainPageProduction from "../components/MainPageProduction/MainPageProduction";
 import Manufacturers from "../components/Manufacturers/Manufacturers";
+import Instruments from "../components/Instruments/Instruments";
 
 const MainPage = () => {
     return (
         <div className="main-page">
             <MainPageProduction/>
             <Manufacturers/>
+            <Instruments/>
         </div>
     );
 };

e20c7066a5a24c60e1d63270070356051fb468c6
lera <lera.24054@gmail.com>
2023-01-29 21:23:09 +0600

    added admin check

--- frontend/src/components/UI/Header/HeaderMobile.js
+++ frontend/src/components/UI/Header/HeaderMobile.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo-mobile.png';
 import cart from '../../../assets/svg/cart.svg';
-import AppDrawer from "../AppDrawer/AppDrawer";
-import Backdrop from "../Backdrop/Backdrop";
+import AppDrawer from '../AppDrawer/AppDrawer';
+import Backdrop from '../Backdrop/Backdrop';
+import { useSelector } from 'react-redux';
 
 const HeaderMobile = ({ mainPage }) => {
+    const user = useSelector((state) => state.users.user);
     const classes = mainPage ? ['header', 'header--main-page'] : ['header'];
 
     return (
         <>
-            <Backdrop/>
+            <Backdrop />
             <div className={classes.join(' ')}>
                 <div className="container-sm">
                     <div className="header__mobile">
@@ -25,20 +27,21 @@ const HeaderMobile = ({ mainPage }) => {
                             />
                         </Link>
                         <div className="header__user-icons">
-                            <Link to="/cart" className="clickable">
-                                <img
-                                    className="header__user-icon"
-                                    src={cart}
-                                    alt="Корзина"
-                                />
-                            </Link>
-                            <AppDrawer/>
+                            {!user || user?.role !== 'admin' ? (
+                                <Link to="/cart" className="clickable">
+                                    <img
+                                        className="header__user-icon"
+                                        src={cart}
+                                        alt="Корзина"
+                                    />
+                                </Link>
+                            ) : null}
+                            <AppDrawer />
                         </div>
                     </div>
                 </div>
             </div>
         </>
-
     );
 };
 

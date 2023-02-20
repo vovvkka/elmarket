2b9a8ccdf4ef6cf3413b301cdecbe5825c7c347c
lera <lera.24054@gmail.com>
2023-02-20 21:58:25 +0600

    prod fixes for zakazchik

--- frontend/src/containers/Contacts.js
+++ frontend/src/containers/Contacts.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../store/actions/contactsActions';
 
 const Contacts = () => {
+    const dispatch = useDispatch();
+    const contacts = useSelector((state) => state.contacts.contacts);
+
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, []);
+
     return (
         <div className="contacts">
             <div className="container-sm">
                 <div className="contacts__block">
                     <div className="contacts__aboutUs">
-                        <h2 className="contacts__title">
-                            О компании
-                        </h2>
+                        <h2 className="contacts__title">О компании</h2>
 
                         <ul className="contacts__list">
-                            <li>ОсОО “Има Электро”</li> <br/>
-
-                            <li>+(996) 777-77-11-07</li>
-                            <li>+(996)709-40-39-55</li>
-                            <li>Electromarket.kg@gmail.com</li> <br/>
-
-                            <li>Юр.адрес: УГНС Ленинского района, код 002</li>
-                            <li>Код ОКПО 30023004</li> <br/>
-
+                            <li>ОсОО “Има Электро”</li> <br />
+                            {contacts.phone.map((ph) => (
+                                <li key={ph}>{ph}</li>
+                            ))}
+                            {contacts.email.map((ph) => (
+                                <li key={ph}>{ph}</li>
+                            ))}                            <li>Юр.адрес: УГНС Ленинского района, код 002</li>
+                            <li>Код ОКПО 30023004</li> <br />
                             <li>ИНН01603201810293</li>
                             <li>ОЗО ДКИБ-ЦЕНТР</li>
                             <li>Р/С 1180000107669503</li>
@@ -29,16 +35,16 @@ const Contacts = () => {
                     <div className="contacts__orders">
                         <div className="contacts__orders-block">
                             <p>
-                                ПРИЕМ ЗАКАЗОВ И ИХ ОБРАБОТКА: <br/>
+                                ПРИЕМ ЗАКАЗОВ И ИХ ОБРАБОТКА: <br />
                                 понедельник - пятница с 9:30 до 18:30
                             </p>
                             <p>
-                                суббота с 10:00 до 18:00 <br/>
+                                суббота с 10:00 до 18:00 <br />
                                 воскресенье выходной
                             </p>
                             <p>
-                                ПРИЕМ ЗАКАЗОВ ONLINE: <br/>
-                                Сделать заказ на нашем сайте <br/>
+                                ПРИЕМ ЗАКАЗОВ ONLINE: <br />
+                                Сделать заказ на нашем сайте <br />
                                 Вы можете круглосуточно.
                             </p>
                         </div>
